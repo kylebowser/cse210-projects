5749198f17b633d44df2561ed73224d6971389fe
kylebowser <kylebowser9@gmail.com>
2025-09-06 14:57:08 -0600

    Week 1 Assignment 1 Exersize 3 Complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.Write("Guess the number: ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        while (guess != number)
+        {
+            if (guess > number)
+            {
+                Console.WriteLine("Smaller");
+            }
+            else
+            {
+                Console.WriteLine("Greater");
+            }
+
+            Console.Write("What is your guess: ");
+            guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+        }
+        Console.WriteLine("You guessed it!");
     }
 }

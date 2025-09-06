76607ae0eaafeb4b332bfaa0bc2150c35630de7c
kylebowser <kylebowser9@gmail.com>
2025-09-06 14:28:59 -0600

    Week 1 Assignment 1 Exersize 2 Complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        string letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a {letterGrade}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats on passing!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }

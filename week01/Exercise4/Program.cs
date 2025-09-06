3d453c767e0d89fee8eb577231eebba5b2d57997
kylebowser <kylebowser9@gmail.com>
2025-09-06 15:45:37 -0600

    Week 1 Assignment 1 Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+
+        if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int part in numbers)
+        {
+            sum += part;
+        }
+
+        Console.WriteLine($"Sum is {sum}");
+
+        float count = numbers.Count;
+        float average = sum / count;
+
+        Console.WriteLine($"Average is {average}");
+
+        int largest = -1;
+
+        foreach (int part in numbers)
+        {
+            if (part > largest)
+            {
+                largest = part;
+            }
+        }
+
+        Console.WriteLine($"Largest is {largest}");
     }
 }

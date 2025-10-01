ff4f978bb1e5e9e4cc020268a3b8474528b29f4b
kylebowser <kylebowser9@gmail.com>
2025-09-30 18:55:09 -0600

    Week 5 Homework

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return ($"{_studentName} - {_topic}");
+    }
+    
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        MathAssignment assingment1 = new MathAssignment("Roberto Rodriguez", "Fractions",
+        "7.3", "8-19");
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters",
+        "European History", "The Causes of World War II");
+
+        Console.WriteLine(assingment1.GetSummary());
+        Console.WriteLine(assingment1.GetHomeworkList());
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }

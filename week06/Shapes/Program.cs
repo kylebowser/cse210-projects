b8ecf0fcbfb4fea7fa7e27b4e84fb8bb98cef506
kylebowser <kylebowser9@gmail.com>
2025-10-09 20:04:18 -0600

    Week 6 Shapes Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Rectangle rectangle = new Rectangle("blue", 2, 5);
+        shapes.Add(rectangle);
+
+        Square square = new Square("black", 12);
+        shapes.Add(square);
+
+        Circle circle = new Circle("green", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+
+
     }
 }
---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Square : Shape
+{
+    double _side;
+
+    public Square(string color, double side): base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
